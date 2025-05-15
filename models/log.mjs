585497dc927aaa471c54461d3bdcb07ad030501d
Santiago Gonzales <anonimodusuario43@gmail.com>
2025-05-15 12:21:57 -0500

    agege el schema de mis logs en models log

--- models/log.mjs
+++ models/log.mjs
@@ -1,7 +1,4 @@
-
 import mongoose from "mongoose";
-
-
 
 const logSchema = new mongoose.Schema({
   timestamp : {
@@ -10,7 +7,45 @@ const logSchema = new mongoose.Schema({
     required: true
   },
   nivel: {
-    enum: ['WARNING']
+    type: String,
+    required: true,
+    message: `El nivel debe ser uno: 'DEBUG','INFO','WARNING','ERROR','FATAL'`,
+    enum: ['DEBUG','INFO','WARNING','ERROR','FATAL']
+  },
+  mensaje: {
+    type: String,
+    required: true,
+    minLength: 1,
+    message: `El mensaje de log es obligatorio`
+  },
+  origen: {
+    type: String,
+    default: 'system',
+    description: 'Indica el componente o servicio que generó el log'
+  },
+  usuario_id: {
+    type: String,
+    description: 'ID del usuario asociado con la acción (si aplica)'
+  },
+  transaccion_id: {
+    type: String,
+    description: 'ID de la transacción o petición (para rastreo)'
+  },
+  detalles: {
+    type: mongoose.Schema.Types.Mixed,
+    description: 'Información adicional estructurada relacionada con el log (opcional)'
+  },
+  error: {
+    type: mongoose.Schema({
+      name: String,
+      message: String,
+      stack: String
+    }),
+    description: 'Objeto que contiene detalles del error (si el nivel es ERROR o FATAL)'
   }
 
 })
+
+const Log = mongoose.model('Log', logSchema, 'logs')
+
+export default Log
